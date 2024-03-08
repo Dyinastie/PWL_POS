db8501b528c9b3c1040200e880956c56f4fe19fe
Dyinastie Marchelina Puspitawening <dyinastiemp@gmail.com>
2024-03-08 08:54:26 +0700

    Jobsheet 4 : $fillable - Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,19 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+            // 'level_id' => 2,
+            // 'username' => 'manager_dua',
+            // 'nama' => 'Manager 2',
+            // 'password' => Hash::make('12345'),
 
-        //coba akses model UserModel
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data);
+
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
