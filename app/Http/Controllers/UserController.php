009b6f3e0c85e4b3818c3841284b19a60cd9fa17
Dyinastie Marchelina Puspitawening <dyinastiemp@gmail.com>
2024-03-08 09:11:30 +0700

    Jobsheet 4 : Retrieving Single Models - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,20 +10,39 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            // 'level_id' => 2,
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            // 'password' => Hash::make('12345'),
+        //======================= PRAKTIKUM 1 ==============================
 
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     // 'level_id' => 2,
+        //     // 'username' => 'manager_dua',
+        //     // 'nama' => 'Manager 2',
+        //     // 'password' => Hash::make('12345'),
 
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+
+        //======================= PRAKTIKUM 2 ==============================
+
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
