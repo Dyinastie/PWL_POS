ca58f08ba9858b4cdf664832abee088b1e55ae38
Dyinastie Marchelina Puspitawening <dyinastiemp@gmail.com>
2024-05-31 10:31:51 +0700

    Jobsheet 12 : D. Memindah File Upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,17 +6,25 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
-    public function fileUpload() {
+    public function fileUpload()
+    {
         return view('file-upload');
     }
 
-    public function prosesFileUpload(Request $request) {
+    public function prosesFileUpload(Request $request)
+    {
         // dump($request->berkas);
         // return "Pemrosesan file upload di sini";
 
         $request->validate([
-            'berkas'=>'required|file|image|max:500'
+            'berkas' => 'required|file|image|max:500'
         ]);
-        echo $request->berkas->getClientOriginalName()." lolos validasi";
+        // $path = $request->berkas->store('uploads');
+        // $path = $request->berkas->storeAs('uploads', 'berkas');
+        $extfile = $request->berkas->getClientOriginalName();
+        $namaFile = 'web-'.time().".".$extfile;
+        $path = $request->berkas->storeAs('uploads', $namaFile);
+        echo "Proses upload berhasil, file berada di: $path";
+        // echo $request->berkas->getClientOriginalName()." lolos validasi";
     }
 }
